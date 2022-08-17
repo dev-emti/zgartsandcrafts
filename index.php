21f4873db63353aa5126e666f20faf92b0ad74d8
dev-emti <dev.emti@gmail.com>
2022-08-17 22:28:09 +0800

    up

---
+++ index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZG ARTS AND CRAFTS</title>
+
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="nav">
+        <div class="logo-container">
+            <img src="./styles/imgs/logo-container.svg" alt="">
+            <ul class="menus">
+                <li class="menu active">HOME</li>
+                <li class="menu">CATEGORY</li>
+                <li class="menu">ABOUT</li>
+            </ul>
+        </div>
+        
+        <div class="links">
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+        </div>
+    </nav>
+
+    <!-- HERO -->
+    <section id="hero">
+        <div class="hero-banner">
+            <img class="hero-bg" src="./styles/imgs/hero-bg.svg" alt="">
+            <img class="hero-main" src="./styles/imgs/hero-main.png" alt="">
+        </div>
+        <div class="hero-text">
+            <h1>ZG ARTS AND CRAFTS</h1>
+            <p>We provide clean and unique designs for your project.</p>
+            <button>SHOP NOW</button>
+        </div>
+        <div class="circle1"></div>
+        <div class="circle2"></div>
+        <div class="circle3"></div>
+    </section>
+
+    <!-- Collection -->
+    <section id="collections">
+        <div class="title">
+            <img src="./styles/imgs/star.png" alt="icon">
+            <h1>Best Seller</h1>
+        </div>
+        <div class="product-container">
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/pink-baby-pig.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Pink Baby Pig Svg | ANIMALS</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/holidays.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Days To Go Before Christmas | CHRISTMAS | FREE SVG FILES</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/sports-icon.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Game Day Svg | Football Svg | SPORTS</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/pink-baby-pig.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Pink Baby Pig Svg | ANIMALS</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="collection-lists">
+        <div class="title">
+            <img src="./styles/imgs/gallery.png" alt="icon">
+            <h1>Collection List</h1>
+        </div>
+        <div class="product-container">
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/pink-baby-pig.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Pink Baby Pig Svg | ANIMALS</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/holidays.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Days To Go Before Christmas | CHRISTMAS | FREE SVG FILES</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/sports-icon.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Game Day Svg | Football Svg | SPORTS</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+            <div class="product-item">
+                <div class="img-container">
+                    <img src="./styles/imgs/pink-baby-pig.png" alt="">
+                </div>
+                <div class="item-info">
+                    <h3>Pink Baby Pig Svg | ANIMALS</h3>
+                    <p>$1.50</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about-us">
+        <div class="title">
+            <img src="./styles/imgs/info.png" alt="icon">
+            <h1>About Us</h1>
+        </div>
+        <div class="about">
+            <img src="./styles/imgs/logo-container.svg" alt="">
+            <div class="info">
+                <h1>ZG ARTS AND CRAFTS</h1>
+                <p>Hey there people!</p>
+                <p>We are a small business owner who aim to help creative crafters to make beautiful and special crafts to sell.</p>
+                <p>We provide clean and unique designs for your project.</p>
+                <p>Whether you're a Cricut or Silhouette user, our cut files work perfectly with any cutting machine.</p>
+                <p>Let's craft together.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact-us">
+        <div class="title">
+            <img src="./styles/imgs/info-colored.png" alt="icon">
+            <h1>Contact Us</h1>
+        </div>
+        <form action="">
+            <h2>FORM</h2>
+            <div class="input-group">
+                <label for="name">Name</label>
+                <input type="text" name="" id="name" placeholder="ex: juan delacruz">
+            </div>
+            <div class="input-group">
+                <label for="email">Email</label>
+                <input type="email" name="" id="email" placeholder="ex: sample@gmail.com">
+            </div>
+            <div class="input-group">
+                <label for="message">Message</label>
+                <textarea name="" id="" cols="30" rows="5"></textarea>
+            </div>
+        </form>
+    </section>
+
+    <footer>
+        <div class="helpful">
+            <h1>Helpful Information</h1>
+            <ul>
+                <li>Refund Policy</li>
+                <li>Terms of Service</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div class="followus">
+            <h1>Follow Us</h1>
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Pinterest</li>
+            </ul>
+        </div>
+    </footer>
+    <div class="copyright">
+        <h1>© 2021, ZGArtsandCrafts</h1>
+    </div>
+</body>
+</html>
